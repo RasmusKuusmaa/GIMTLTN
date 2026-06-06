630207dd33119006ba1594248e311e014aacb7f5
Rasmus Kuusmaa <77f315@gmail.com>
2026-06-06 19:04:57 +0300

    player animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,12 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var player: CharacterBody2D = $"."
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -21,5 +24,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+	#animation
+	if direction != 0:
+		animated_sprite_2d.play("Run")
+		animated_sprite_2d.flip_h = direction < 0
+	else:
+		animated_sprite_2d.play("Idle")
